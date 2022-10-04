3f1ca363337086a9fd7280981dfbd0f8d8d038b9
jckhe <81775433+Jckhe@users.noreply.github.com>
2022-10-03 18:57:18 -0700

    added very necessary updates

--- client/components/Cow.js
+++ client/components/Cow.js
@@ -13,8 +13,32 @@ function CowContainer(props) {
           <div onClick={() => {
             dispatch(clickCowActionCreator())
             console.log(store.getState())
-          }} className="hitbox">
+          }} className="hitbox" id="hitbox">
               <img src={'https://i.imgur.com/sQusjAS.png'} alt="" />
+          </div>
+          <div onClick={() => {
+            dispatch(clickCowActionCreator())
+            console.log(store.getState())
+          }} className="hitbox" id="hitbox2">
+              <img height="100px" width="100px" id="extraCows" src={'https://i.imgur.com/P8XvrPJ.jpg'} alt="" />
+          </div>
+          <div onClick={() => {
+            dispatch(clickCowActionCreator())
+            console.log(store.getState())
+          }} className="hitbox" id="hitbox3">
+              <img height="100px" width="100px" id="extraCows" src={'https://i.imgur.com/gX58cIY.png'} alt="" />
+          </div>
+          <div onClick={() => {
+            dispatch(clickCowActionCreator())
+            console.log(store.getState())
+          }} className="hitbox" id="hitbox4">
+              <img height="100px" width="100px" id="extraCows" src={'https://i.imgur.com/rlULwYi.png'} alt="" />
+          </div>
+          <div onClick={() => {
+            dispatch(clickCowActionCreator())
+            console.log(store.getState())
+          }} className="hitbox" id="hitbox5">
+              <img height="100px" id="extraCows" width="100px" src={'https://i.imgur.com/fqmapJJ.png'} alt="" />
           </div>
       </div>
   )
